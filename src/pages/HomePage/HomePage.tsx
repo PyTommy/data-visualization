f0088faf484320ac1678c0c0dcd2886a04928434
Hiroki Tominaga <hiroki.engineer@gmail.com>
2021-11-28 20:26:18 +0900

    created PageContainer

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,17 +2,18 @@ import { Typography } from '@mui/material'
 import React from 'react'
 
 import { SampleColorsAPI } from 'api'
+import { PageContainer } from 'components/PageContainer'
 import { useAsync } from 'hooks/useAsync'
 
 export const HomePage = () => {
   const { state } = useAsync(() => SampleColorsAPI.getCsv(), [])
   return (
-    <div>
+    <PageContainer>
       <Typography variant="h2">ホーム</Typography>
       <div>
         <h2>Data</h2>
         {JSON.stringify(state.value, null, 4)}
       </div>
-    </div>
+    </PageContainer>
   )
 }
